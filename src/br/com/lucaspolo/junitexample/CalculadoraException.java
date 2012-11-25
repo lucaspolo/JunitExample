90b4a7ad9cf627bee3414ef08626b5506bc0e299
Lucas Polo <lucaspolo@lucaspolo.com.br>
2012-11-25 08:58:24 -0200

    Adicionando serialVersionUID padrão na classe CalculadoraException

--- src/br/com/lucaspolo/junitexample/CalculadoraException.java
+++ src/br/com/lucaspolo/junitexample/CalculadoraException.java
@@ -1,6 +1,11 @@
 package br.com.lucaspolo.junitexample;
 
 public class CalculadoraException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public CalculadoraException() {
 		// TODO Auto-generated constructor stub
 	}
