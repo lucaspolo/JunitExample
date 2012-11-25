336e12b62060222c60434739b0a43090f12bf5c3
Lucas Polo <lucaspolo@lucaspolo.com.br>
2012-11-25 08:36:57 -0200

    Criação do método setUp em CalculadoraTeste para inicializar a calculadora

--- src/br/com/lucaspolo/junitexample/teste/CalculadoraTeste.java
+++ src/br/com/lucaspolo/junitexample/teste/CalculadoraTeste.java
@@ -2,24 +2,28 @@ package br.com.lucaspolo.junitexample.teste;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.com.lucaspolo.junitexample.Calculadora;
 
 public class CalculadoraTeste {
+	Calculadora calculadora;
+	
+	@Before
+	public void setUp() {
+		calculadora = new Calculadora();
+	}
 
 	@Test
 	public void somaTeste() {
-		Calculadora calculadora = new Calculadora();
 		int resultado = calculadora.soma(5,4);
 		assertEquals(9, resultado);
 	}
 	
 	@Test
 	public void somaTestePontoFlutuante() {
-		Calculadora calculadora = new Calculadora();
 		double resultado = calculadora.soma(4.3, 2.2);
 		assertEquals(6.5, resultado, 0.1);
 	}
-
 }
